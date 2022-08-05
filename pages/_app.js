8074fbdd545b1c9991ab658fd1fa364fac9e35bf
mrzmyr <moritz@devmood.com>
2022-08-05 17:09:46 +0200

    feat: add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 import Head from "next/head";
+import Script from 'next/script'
 
 function App({ Component, pageProps }) {
   return (
     <>
       <Head>
         <link rel="shortcut icon" href="/images/favicon.png" />
+        <Script async src="https://cdn.panelbear.com/analytics.js?site=4Jdh7AyQ4x" />
+        <Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.panelbear = window.panelbear || function() { (window.panelbear.q = window.panelbear.q || []).push(arguments); };
+              panelbear('config', { site: '4Jdh7AyQ4x' });
+            `,
+          }}
+        />
       </Head>
       <Component {...pageProps} />
     </>
