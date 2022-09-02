376a18bbbae7505e3d30443c80bfb7e41bab9523
mrzmyr <moritz@devmood.com>
2022-09-02 20:53:54 +0200

    feat: add posthog

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,17 @@
 import '../styles/globals.css'
 
+import { usePostHog } from 'next-use-posthog'
 import Head from "next/head";
-import Script from 'next/script'
 
 function App({ Component, pageProps }) {
+  usePostHog('phc_GBlaJ7lH6EM1dxaXsH7D3iBaHE2OnnW9xHmSZOgmd88', { api_host: 'https://app.posthog.com' })
+
   return (
     <>
       <Head>
         <link rel="shortcut icon" href="/images/favicon.png" />
       </Head>
       <Component {...pageProps} />
-      <Script async src="https://cdn.panelbear.com/analytics.js?site=4Jdh7AyQ4x" />
-      <Script
-        id="panelbear"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.panelbear = window.panelbear || function() { (window.panelbear.q = window.panelbear.q || []).push(arguments); };
-            if(panelbear) panelbear('config', { site: '4Jdh7AyQ4x' });
-          `,
-        }}
-      />
     </>
   );
 }
