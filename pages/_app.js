dfaa5e6149a48a027953fdeedbe13cf4c95f47c7
mrzmyr <moritz@devmood.com>
2022-09-02 20:56:21 +0200

    feat: disable posthog in dev

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,12 @@ import { usePostHog } from 'next-use-posthog'
 import Head from "next/head";
 
 function App({ Component, pageProps }) {
-  usePostHog('phc_GBlaJ7lH6EM1dxaXsH7D3iBaHE2OnnW9xHmSZOgmd88', { api_host: 'https://app.posthog.com' })
+  usePostHog('phc_GBlaJ7lH6EM1dxaXsH7D3iBaHE2OnnW9xHmSZOgmd88', {
+    api_host: 'https://app.posthog.com',
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.opt_out_capturing()
+    },
+  })  
 
   return (
     <>
