75a05f90a8e96a141634ec27e02fad5f74d445bb
mrzmyr <moritz@devmood.com>
2022-10-03 15:03:22 +0200

    feat: add new questions (now in yml)

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    config.module.rules.push(
+      {
+        test: /\.ya?ml$/,
+        use: 'js-yaml-loader',
+      },
+    )
+    return config
+  },
+
 }
 
 module.exports = nextConfig
--- pages/api/questions.ts
+++ pages/api/questions.ts
@@ -15,49 +15,8 @@ type Question = {
   }[];
 };
 
-const q = {
-  id: 1,
-  text: {
-    en: 'Do you like the new update?',
-    de: 'Magst du das neue Update?',
-  },
-  type: 'single',
-  answers: [
-    { 
-      id: 1, 
-      text: {
-        en: 'I love it',
-        de: 'Ich liebe es',
-      }, 
-      emoji: '😍'
-    },
-    { 
-      id: 2, 
-      text: {
-        en: 'Its ok',
-        de: 'Ist ok',
-      }, 
-      emoji: '👍' 
-    },
-    {
-      id: 3,
-      text: {
-        en: `I'm experiencing some issues`,
-        de: 'Es gibt ein paar Fehler',
-      },
-      emoji: '😕',
-    },
-    { 
-      id: 4, 
-      text: {
-        en: 'Not really',
-        de: 'Nicht wirklich',
-      }, 
-      emoji: '👎' 
-    },
-  ],
-}
+import questions from '../../store/questions.yml';
 
 export default function handler(req, res) {
-  res.status(200).json([q] as Question[])
+  res.status(200).json(questions as Question[])
 }
