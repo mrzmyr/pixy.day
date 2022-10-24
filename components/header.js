c6472a11bf8be9f55f02f9134d4691f4b10fd6f7
mrzmyr <moritz@devmood.com>
2022-10-24 23:04:55 +0200

    feat: add 'submit feature' link to header

--- components/header.js
+++ components/header.js
@@ -1,25 +1,27 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header className='flex flex-row items-center'>
-      <div className='mr-14'>
-        <Link href='/' passHref>
-          <a className='flex flex-row items-center'>
-            <Image
-              alt={'Pixy'}
-              width={44}
-              height={44}
-              src="/images/icon.png"
-            />
-            <div className='text-2xl font-bold ml-2'>Pixy</div>
+    <header className="flex flex-row items-center">
+      <div className="mr-14">
+        <Link href="/" passHref>
+          <a className="flex flex-row items-center">
+            <Image alt={"Pixy"} width={44} height={44} src="/images/icon.png" />
+            <div className="text-2xl font-bold ml-2">Pixy</div>
           </a>
         </Link>
       </div>
       {/* <Link href="/" passHref><a className='mr-4 lg:mr-12 font-medium'>Home</a></Link> */}
-      {/* <Link href="/" passHref><a className='mr-4 lg:mr-12 font-medium'>Feature Ideas</a></Link>
-      <Link href="/" passHref><a className='mr-4 lg:mr-12 font-medium'>Changelog</a></Link> */}
+      {/* {/* <Link href="/" passHref><a className='mr-4 lg:mr-12 font-medium'>Feature Ideas</a></Link> */}
+      <a
+        href="https://pixy.hellonext.co"
+        target={"_blank"}
+        rel="noreferrer"
+        className="mr-4 lg:mr-12 font-medium cursor-pointer"
+      >
+        Submit Feature
+      </a>
     </header>
- )
-}
+  );
+};
