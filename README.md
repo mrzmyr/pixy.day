183964c9695f38a5ce02ecabcb85d1e2d033e206
mrzmyr <moritz@devmood.com>
2022-11-05 23:59:53 +0100

    feat: Remove questions

--- README.md
+++ README.md
@@ -15,3 +15,15 @@
     <a href="https://github.com/mrzmyr/pixy.day/issues">Request Feature</a>
   </p>
 </p>
+
+### Questions
+
+- Q3: We listened: You can choose your color scheme in the settings now.
+- Q4: We listened: You can choose your color scheme in the settings now.
+- Q5: Would you be interested in sharing your mood with your partner/friend?
+- Q6: Would it be helpful for you to track 'emotions' in a dedicated step?
+- Q7: Would it be beneficial, if you could optionally disable the 'tags' step?
+- Q8: Would it be beneficial, if you could optionally disable the 'write about your day' step?
+- Q10: Would you be interested in tracking your sleep along with your mood?
+- Q11: Would you be interested in a online backup funtionalitiy?
+- Q12: Do you want weekly notifications about your statistics?
--- store/questions.ts
+++ store/questions.ts
@@ -61,135 +61,6 @@ const questions: Question[] = [
     ]
   },
   {
-    "id": "q3",
-    "text": {
-      "en": "We listened: You can choose your color scheme in the settings now.",
-      "de": "Wir hören dich: Du kannst nun dein Farbschema in den Einstellungen ändern."
-    },
-    "type": "single",
-    appVersion: ">= 1.44.0",
-    "answers": [
-      {
-        "id": 1,
-        "text": {
-          "en": "Awesome! Will do right now.",
-          "de": "Klasse, mach ich sofort!"
-        },
-        "emoji": "🤩"
-      },
-      {
-        "id": 2,
-        "text": {
-          "en": "I will take a look.",
-          "de": "Ich schau mal."
-        },
-        "emoji": "🥸"
-      },
-      {
-        "id": 3,
-        "text": {
-          "en": "Already did it!",
-          "de": "Hab ich schon"
-        },
-        "emoji": "😋"
-      },
-      {
-        "id": 4,
-        "text": {
-          "en": "Not interested.",
-          "de": "Interessiert mich nicht."
-        },
-        "emoji": "🙂"
-      }
-    ]
-  },
-  {
-    "id": "q4",
-    "text": {
-      "en": "We listened: You can choose your color scheme in the settings now.",
-      "de": "Wir hören dich: Du kannst nun dein Farbschema in den Einstellungen ändern."
-    },
-    "type": "single",
-    "appVersion": ">= 1.46.1",
-    "answers": [
-      {
-        "id": 1,
-        "text": {
-          "en": "Awesome! Will do right now.",
-          "de": "Klasse, mach ich sofort!"
-        },
-        "emoji": "🤩"
-      },
-      {
-        "id": 2,
-        "text": {
-          "en": "I will take a look.",
-          "de": "Ich schau mal."
-        },
-        "emoji": "🥸"
-      },
-      {
-        "id": 3,
-        "text": {
-          "en": "Already did it!",
-          "de": "Hab ich schon"
-        },
-        "emoji": "😋"
-      },
-      {
-        "id": 4,
-        "text": {
-          "en": "Not interested.",
-          "de": "Interessiert mich nicht."
-        },
-        "emoji": "🙂"
-      }
-    ]
-  },
-  {
-    "id": "q5",
-    "text": {
-      "en": "Would you be interested in sharing your mood with your partner/friend?",
-      "de": "Wärst du daran interessiert, deine Stimmung mit Partner*in/Freund*in zu teilen?"
-    },
-    "type": "single",
-    "appVersion": ">= 1.46.1",
-    "answers": [
-      {
-        "id": 1,
-        "text": {
-          "en": "Yes with my partner.",
-          "de": "Ja mit Partner*in"
-        },
-        "emoji": "❤️"
-      },
-      {
-        "id": 2,
-        "text": {
-          "en": "Yes with friends",
-          "de": "Ja mit Freund*innen"
-        },
-        "emoji": "🤝"
-      },
-      {
-        "id": 3,
-        "text": {
-          "en": "Yes with other than friends/partner",
-          "de": "Ja, anderen als Freund*innen/Partner*innen"
-        },
-        "emoji": "🤗"
-      },
-      {
-        "id": 4,
-        "text": {
-          "en": "No, I would not share my mood.",
-          "de": "Nein, ich würde meine Stimmung nicht teilen."
-        },
-        "emoji": "👎"
-      }
-    ]
-  },
-  {
     "id": "q6",
     "text": {
       "en": "Would it be helpful for you to track 'emotions' in a dedicated step?",
@@ -256,6 +127,14 @@ const questions: Question[] = [
           "de": "Nicht sicher"
         },
         "emoji": "🤔"
+      },
+      {
+        "id": 4,
+        "text": {
+          "en": "I want to see less questions",
+          "de": "Ich möchte weniger Fragen sehen"
+        },
+        "emoji": "🙈"
       }
     ]
   },
@@ -291,7 +170,7 @@ const questions: Question[] = [
           "de": "Nicht sicher"
         },
         "emoji": "🤔"
-      }
+      },
     ]
   },
   {
@@ -326,6 +205,14 @@ const questions: Question[] = [
           "de": "Nicht sicher"
         },
         "emoji": "🤔"
+      },
+      {
+        "id": 4,
+        "text": {
+          "en": "I want to see less questions",
+          "de": "Ich möchte weniger Fragen sehen"
+        },
+        "emoji": "🙈"
       }
     ]
   },
@@ -396,6 +283,14 @@ const questions: Question[] = [
           "de": "Nicht sicher"
         },
         "emoji": "🤔"
+      },
+      {
+        "id": 4,
+        "text": {
+          "en": "I want to see less questions",
+          "de": "Ich möchte weniger Fragen sehen"
+        },
+        "emoji": "🙈"
       }
     ]
   }
